163f8c3ebff19179452ce221fdfa36d947534d98
Anderson Affonso Bonde <andersonbonde@gmail.com>
2023-07-27 15:42:13 -0300

    Refactor some displayController functions

--- src/displayController.js
+++ src/displayController.js
@@ -3,99 +3,118 @@ import todoItem from "./todo-item";
 
 const displayController = (() => {
     const cache = cacheDOM();
-    let currentDisplayedItem = undefined;
+    let currentDisplayedItemId = undefined;
 
     addListeners(cache);
+    
+    function cacheDOM() {
+        const obj = {};
+    
+        obj.currentList = document.querySelector(".current-list");
+        obj.addNewTaskButton = document.querySelector("#add-new-task");
+        obj.newTaskCard = document.querySelector(".task");
+        obj.taskTitleField = document.querySelector("#title");
+        obj.taskDescriptionField = document.querySelector("#description");
+        obj.taskListField = document.querySelector("#list");
+        obj.taskDateField = document.querySelector("#due-date");    
+        obj.taskDeleteButton = document.querySelector("#delete");
+        obj.taskSaveChangeButton = document.querySelector("#save-change");
+    
+        return obj;
+    }
+    
+    function addListeners(obj) {
+        obj.addNewTaskButton.addEventListener("click", openCleanTaskCard.bind(null, obj));
+        obj.taskDeleteButton.addEventListener("click", closeTaskCard.bind(null, obj));
+        obj.taskSaveChangeButton.addEventListener("click", handleSaveChange.bind(null, obj));
+    }
+    
+    function openCleanTaskCard(obj) {
+        clearTaskCard(obj);
+        
+        obj.newTaskCard.style.display = "flex";
+    }
+    
+    function openTaskCardWithInfo(obj, id, item) {
+        clearTaskCard(obj);
+    
+        obj.newTaskCard.style.display = "flex";
+    
+        loadItemToCard(obj, item);
+        displayController.currentDisplayedItemId = id;
+    }
+    
+    function clearTaskCard(obj) {
+        obj.taskTitleField.value = "";
+        obj.taskDescriptionField.value = "";
+        obj.taskListField.value = "";
+        obj.taskDateField.value = "";
+    
+        displayController.currentDisplayedItemId = undefined;
+    }
+    
+    function loadItemToCard(obj, item) {
+        obj.taskTitleField.value = item.getTitle();
+        obj.taskDescriptionField.value = item.getDescription();
+    }
+    
+    function closeTaskCard(obj) {
+        if(displayController.currentDisplayedItemId != undefined) {
+            deleteOpenedTask();
+        }
+    
+        obj.newTaskCard.style.display = "none";
+        displayController.currentDisplayedItemId = undefined;
+    }
+    
+    function deleteOpenedTask() {
+        let target = findOpenedItem();
+    
+        displayController.cache.currentList.removeChild(target);
+    }
+    
+    function findOpenedItem() {
+        let id = displayController.currentDisplayedItemId;
+        let allTasks = document.querySelectorAll(".list-item");
+    
+        return Array.from(allTasks).find(curr => curr.dataset.id == id);
+    }
+    
+    function handleSaveChange(obj) {
+        if(displayController.currentDisplayedItemId != undefined) {
+            modifyCurrentItem();
+        } else {
+            createNewListItem(obj);
+        }
+    }
+
+    function modifyCurrentItem() {
+        let target = findOpenedItem();
+        let item = target.dataset.item;
+
+        console.log(item.getDescription());
+    }
+    
+    function createNewListItem(obj) {
+        let newTitle = obj.taskTitleField.value;
+        let newDesc = obj.taskDescriptionField.value;
+    
+        if(newTitle != "") {
+            let newItem = todoItem(newTitle, newDesc);
+            let newListItem = listItem(newItem);
+        
+            obj.currentList.appendChild(newListItem);
+        }
+    
+        closeTaskCard(obj);
+    }
 
     return {
         cache,
-        currentDisplayedItem,
-        openTaskCard
+        currentDisplayedItemId,
+        openTaskCardWithInfo
     }
 })();
-
-
-function cacheDOM() {
-    const obj = {};
-
-    obj.currentList = document.querySelector(".current-list");
-    obj.addNewTaskButton = document.querySelector("#add-new-task");
-    obj.newTaskCard = document.querySelector(".task");
-    obj.taskTitleField = document.querySelector("#title");
-    obj.taskDescriptionField = document.querySelector("#description");
-    obj.taskListField = document.querySelector("#list");
-    obj.taskDateField = document.querySelector("#due-date");    
-    obj.taskDeleteButton = document.querySelector("#delete");
-    obj.taskSaveChangeButton = document.querySelector("#save-change");
-
-    return obj;
-}
-
-function addListeners(obj) {
-    obj.addNewTaskButton.addEventListener("click", openTaskCard.bind(null, obj, undefined));
-    obj.taskDeleteButton.addEventListener("click", closeTaskCard.bind(null, obj));
-    obj.taskSaveChangeButton.addEventListener("click", createNewListItem.bind(null, obj));
-}
-
-function openTaskCard(obj, item) {
-    clearTaskCard(obj);
-    
-    obj.newTaskCard.style.display = "flex";
-
-    if(item !== undefined) {
-        loadItemToCard(obj, item);
-        displayController.currentDisplayedItem = item;
-    }
-}
-
-function clearTaskCard(obj) {
-    obj.taskTitleField.value = "";
-    obj.taskDescriptionField.value = "";
-    obj.taskListField.value = "";
-    obj.taskDateField.value = "";
-
-    displayController.currentDisplayedItem = undefined;
-}
-
-function loadItemToCard(obj, item) {
-    obj.taskTitleField.value = item.getTitle();
-    obj.taskDescriptionField.value = item.getDescription();
-}
-
-function closeTaskCard(obj) {
-    if(displayController.currentDisplayedItem != undefined) {
-        deleteOpenedTask();
-    }
-
-    obj.newTaskCard.style.display = "none";
-    displayController.currentDisplayedItem = undefined;
-}
-
-function deleteOpenedTask() {
-    let target = findOpenedItem(displayController.currentDisplayedItem);
-
-    displayController.cache.currentList.removeChild(target);
-}
-
-function findOpenedItem(item) {
-    let allTasks = document.querySelectorAll(".list-item");
-
-    return Array.from(allTasks).find(curr => curr.dataset.item == item);
-}
-
-function createNewListItem(obj) {
-    let newTitle = obj.taskTitleField.value;
-    let newDesc = obj.taskDescriptionField.value;
-
-    if(newTitle != "" && newDesc != "") {
-        let newItem = todoItem(newTitle, newDesc);
-        let newListItem = listItem(newItem);
-    
-        obj.currentList.appendChild(newListItem);
-    }
-
-    closeTaskCard(obj);
-}
 
 export default displayController;
 
--- src/list-item.js
+++ src/list-item.js
@@ -2,69 +2,69 @@ import displayController from "./displayController";
 
 const listItem = (todoItem) => {
     const elements = createElements();
-    const _item = todoItem;
+    const item = todoItem;
+    const id = item.getTitle();
 
-    addClassesAndData(elements, _item);
+    addClassesAndData(elements, id);
     parentChildStructure(elements);
     addText(elements, todoItem);
-    addListenersToButtons(elements, _item);
+    addListenersToButtons(elements, id, todoItem);
+    
+    function createElements() {
+        const obj = {};
+    
+        obj.container = document.createElement("div");
+        obj.title = document.createElement("h3");
+        obj.description = document.createElement("p");
+        obj.buttonContainer = document.createElement("div");
+        obj.deleteButton = document.createElement("button");
+        obj.viewButton = document.createElement("button");
+    
+        return obj;
+    }
+    
+    function addClassesAndData(obj, id) {
+        obj.container.classList.add("list-item");
+        obj.container.dataset.id = id;
+    
+        obj.title.classList.add("list-item-title");
+        obj.description.classList.add("list-item-description");
+        obj.buttonContainer.classList.add("list-item-buttons");
+        obj.deleteButton.classList.add("list-item-delete");
+        obj.viewButton.classList.add("list-item-view");
+    }
+    
+    function parentChildStructure(obj) {
+        obj.container.appendChild(obj.title);
+        obj.container.appendChild(obj.description);
+        obj.container.appendChild(obj.buttonContainer);
+        
+        obj.buttonContainer.appendChild(obj.deleteButton);
+        obj.buttonContainer.appendChild(obj.viewButton);
+    }
+    
+    function addText(obj, item) {
+        obj.title.textContent = item.getTitle();
+        obj.description.textContent = item.getDescription();
+        obj.deleteButton.textContent = "Delete";
+        obj.viewButton.textContent = "View";
+    }
+    
+    function addListenersToButtons(obj, id, item) {
+        obj.deleteButton.addEventListener("click", deleteSelf.bind(null, obj));
+        obj.viewButton.addEventListener("click", displaySelf.bind(null, obj, id, item));
+    }
+    
+    function deleteSelf(obj) {
+        let parent = obj.deleteButton.closest(".list-item");
+        displayController.cache.currentList.removeChild(parent);
+    }
+    
+    function displaySelf(obj, id, item) {
+        displayController.openTaskCardWithInfo(displayController.cache, id, item);
+    }
 
     return elements.container;
-}
-
-function createElements() {
-    const obj = {};
-
-    obj.container = document.createElement("div");
-    obj.title = document.createElement("h3");
-    obj.description = document.createElement("p");
-    obj.buttonContainer = document.createElement("div");
-    obj.deleteButton = document.createElement("button");
-    obj.viewButton = document.createElement("button");
-
-    return obj;
-}
-
-function addClassesAndData(obj, item) {
-    obj.container.classList.add("list-item");
-    obj.container.dataset.item = item;
-
-    obj.title.classList.add("list-item-title");
-    obj.description.classList.add("list-item-description");
-    obj.buttonContainer.classList.add("list-item-buttons");
-    obj.deleteButton.classList.add("list-item-delete");
-    obj.viewButton.classList.add("list-item-view");
-}
-
-function parentChildStructure(obj) {
-    obj.container.appendChild(obj.title);
-    obj.container.appendChild(obj.description);
-    obj.container.appendChild(obj.buttonContainer);
-    
-    obj.buttonContainer.appendChild(obj.deleteButton);
-    obj.buttonContainer.appendChild(obj.viewButton);
-}
-
-function addText(obj, item) {
-    obj.title.textContent = item.getTitle();
-    obj.description.textContent = item.getDescription();
-    obj.deleteButton.textContent = "Delete";
-    obj.viewButton.textContent = "View";
-}
-
-function addListenersToButtons(obj, item) {
-    obj.deleteButton.addEventListener("click", deleteSelf.bind(null, obj));
-    obj.viewButton.addEventListener("click", displaySelf.bind(null, obj, item));
-}
-
-function deleteSelf(obj) {
-    let parent = obj.deleteButton.closest(".list-item");
-    displayController.cache.currentList.removeChild(parent);
-}
-
-function displaySelf(obj, item) {
-    displayController.openTaskCard(displayController.cache, item);
-    displayController.currentDisplayedItem = item;
 }
 
 export default listItem;
