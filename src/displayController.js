493f5f6d46399cf66ebb26977e7bbe87991626ba
Anderson Affonso Bonde <andersonbonde@gmail.com>
2023-08-01 12:10:49 -0300

    Add functionality to create and switch lists

--- src/displayController.js
+++ src/displayController.js
@@ -29,6 +29,7 @@ const displayController = (() => {
     
         obj.projectsList = document.querySelector(".projects-list");
         obj.currentList = document.querySelector(".current-list");
+        obj.addNewListButton = document.querySelector("#add-new-list");
         obj.addNewTaskButton = document.querySelector("#add-new-task");
         obj.newTaskCard = document.querySelector(".task");
         obj.taskTitleField = document.querySelector("#title");
@@ -42,24 +43,30 @@ const displayController = (() => {
     }
     
     function addListeners() {
+        cache.addNewListButton.addEventListener("click", createNewList);
         cache.addNewTaskButton.addEventListener("click", openCleanTaskCard);
         cache.taskDeleteButton.addEventListener("click", closeTaskCard);
         cache.taskSaveChangeButton.addEventListener("click", handleSaveChange);
 
         events.on("viewItem", openTaskCardWithInfo);
         events.on("deleteItem", deleteTask);
+
+        document.addEventListener("keydown", enterKeyToSaveChanges);
+        document.addEventListener("keydown", deleteKeyToDeleteOpenTask);
     }
     
     function openCleanTaskCard() {
         clearTaskCard();
-        
+
         cache.newTaskCard.style.display = "flex";
+        cache.taskTitleField.focus();
     }
     
     function openTaskCardWithInfo(id, item) {
         clearTaskCard();
         
         cache.newTaskCard.style.display = "flex";
+        cache.taskTitleField.focus();
     
         loadItemToCard(item);
         _currentDisplayedItemId = id;
@@ -105,7 +112,8 @@ const displayController = (() => {
     }
 
     function removeTaskFromList(item) {
-        events.emit("listItemDeleted", item.dataset.id);
+        let index = _currentList.items.findIndex(curr => curr.id == item.dataset.id);
+        _currentList.items.splice(index, 1);
     }
     
     function findOpenedItem(id) {
@@ -139,25 +147,81 @@ const displayController = (() => {
 
             cache.currentList.appendChild(newListItem.container);
 
-            events.emit("listItemCreated", newListItem);
+            _currentList.items.push(newListItem);
         }
     
         closeTaskCard();
     }
 
     function populateNavBarList() {
+        clearNavBar();
+
         _allLists.forEach(curr => {
             let newLi = document.createElement("li");
             let newA = document.createElement("a");
 
             newLi.appendChild(newA);
+            newLi.classList.add("to-do-list");
 
-            newA.classList.add("to-do-list");
             newA.href = "#";
             newA.textContent = curr.getName();
+            newA.addEventListener("click", setCurrentList.bind(null, curr.getName()));
 
             cache.projectsList.appendChild(newLi);
         })
+    }
+
+    function clearNavBar() {
+        let all = cache.projectsList.querySelectorAll(".to-do-list");
+
+        all.forEach(curr => {
+            cache.projectsList.removeChild(curr);
+        })
+    }
+
+    function setCurrentList(newListName) {
+        let toLoad = _allLists.find(curr => curr.getName() == newListName);
+        _currentList = toLoad;
+
+        document.querySelector(".list-name").textContent = newListName;
+
+        loadCurrentList();
+    }
+
+    function loadCurrentList() {
+        clearAllFromCurrentList();
+
+        _currentList.items.forEach(curr => {
+            cache.currentList.appendChild(curr.container);
+        })
+    }
+
+    function clearAllFromCurrentList() {
+        let allItems = cache.currentList.querySelectorAll(".list-item");
+
+        allItems.forEach(curr => {
+            cache.currentList.removeChild(curr);
+        })
+    }
+
+    function createNewList() {
+        let listName = prompt("List Name?", "");
+        let newList = list(listName);
+        _allLists.push(newList);
+
+        populateNavBarList();
+    }
+
+    function enterKeyToSaveChanges(e) {
+        if(e.code == "Enter" && cache.newTaskCard.style.display == "flex") {
+            createNewListItem();
+        }
+    }
+
+    function deleteKeyToDeleteOpenTask(e) {
+        if(e.code == "Delete" && cache.newTaskCard.style.display == "flex") {
+            closeTaskCard();
+        }
     }
 })();
 
--- src/list.js
+++ src/list.js
@@ -1,26 +1,8 @@
-import events from "./events";
-
 const list = (name) => {
     const items = [];
     let _name = name;
 
     const getName = () => _name;
-
-    addListeners();
-
-    function addListeners() {
-        events.on("listItemCreated", addItemToList);
-        events.on("listItemDeleted", deleteItemFromList);
-    }
-
-    function addItemToList(listItem) {
-        items.push(listItem);
-    }
-
-    function deleteItemFromList(id) {
-        let idIndex = items.findIndex(curr => curr.id == id);
-        items.splice(idIndex, 1);
-    }
 
     return {
         items,
